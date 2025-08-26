3d7f90c1ef4764477829afc4ccec35124a798316
JuliaFranco24 <juliamarquesfa09@gmail.com>
2025-08-26 14:48:27 -0300

    Add files via upload

---
+++ funções3.26.py
@@ -0,0 +1,8 @@
+#Crie uma função que valida uma senha enviada pelousuário
+senha_correta=input("Qual é a senha?")
+
+def senha(senha_correta):
+    if senha_correta=="hamburguer preto":
+        print("Acesso liberado")
+    else:
+        print("Acesso Negado")
